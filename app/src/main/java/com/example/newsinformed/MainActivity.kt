1b60b344816a8cdb295ad7d0b62587c191db928f
Wesley Nova Lima <wnovalim@emeal.nttdata.com>
2022-06-15 17:20:13 -0300

    feat: Fragments and Navigation

--- app/src/main/java/com/example/newsinformed/MainActivity.kt
+++ app/src/main/java/com/example/newsinformed/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.newsinformed
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.newsinformed.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+
+    /* private val binding: ActivityMainBinding by lazy {
+         ActivityMainBinding.inflate(layoutInflater)
+     }*/
+
+    /*private val navHostFragment: NavHostFragment by lazy {
+        supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+    }*/
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.apply { setupWithNavController(navController) }
     }
 }
