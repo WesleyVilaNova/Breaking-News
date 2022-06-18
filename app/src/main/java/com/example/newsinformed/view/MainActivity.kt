6138235206f0b69923ed7a31d60358799f3d6299
Wesley Nova Lima <wnovalim@emeal.nttdata.com>
2022-06-17 21:20:40 -0300

    feat: Config logic search news

--- app/src/main/java/com/example/newsinformed/view/MainActivity.kt
+++ app/src/main/java/com/example/newsinformed/view/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.newsinformed.view
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.newsinformed.R
 import com.example.newsinformed.databinding.ActivityMainBinding
+import com.example.newsinformed.model.repository.IWebService
+import com.example.newsinformed.model.repository.NewsRepository
+import com.example.newsinformed.view.viewmodel.HomeNewsViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private val viewModel: HomeNewsViewModel by viewModels {
+        HomeNewsViewModel.MyHomeViewModelFactory(NewsRepository(IWebService.getInstance()))
+    }
     private lateinit var binding: ActivityMainBinding
     private lateinit var navHostFragment: NavHostFragment
 
@@ -17,7 +24,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupObservers()
         setupContainerFragments()
+    }
+
+    private fun setupObservers() {
+        viewModel.newsResult.observe(this) {
+        }
+        viewModel.errorMsg.observe(this) {
+
+        }
     }
 
     private fun setupContainerFragments() {
--- app/src/main/java/com/example/newsinformed/view/fragments/HomeNewsFragment.kt
+++ app/src/main/java/com/example/newsinformed/view/fragments/HomeNewsFragment.kt
@@ -1,16 +1,61 @@
 package com.example.newsinformed.view.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.example.newsinformed.R
 import com.example.newsinformed.databinding.FragmentHomeListNewsBinding
+import com.example.newsinformed.model.ModelNews
+import com.example.newsinformed.view.viewmodel.HomeNewsViewModel
 
 class HomeNewsFragment : Fragment(R.layout.fragment_home_list_news) {
 
+    companion object {
+        const val TAG: String = ""
+    }
+
     private lateinit var binding: FragmentHomeListNewsBinding
+    private val viewModel: HomeNewsViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding = FragmentHomeListNewsBinding.bind(view)
+
+        setupObservers()
+        loadNewsInit()
+        setupEditTextHome()
+    }
+
+    private fun setupObservers() {
+        viewModel.newsResult.observe(viewLifecycleOwner) { resultNewsOfRequest ->
+            setupAdapter(resultNewsOfRequest)
+        }
+        viewModel.errorMsg.observe(viewLifecycleOwner) {
+        }
+    }
+
+    private fun loadNewsInit() {
+        callingRequestApi("Brazil")
+    }
+
+    private fun setupEditTextHome() {
+        binding.editTextHome.setOnClickListener {
+            val searchText = binding.editTextHome.text.toString()
+            if (searchText.isNotEmpty()) {
+                callingRequestApi(searchText)
+                binding.editTextHome.text.clear()
+            } else {
+                binding.editTextHome.error = "aaaaaaaa"
+            }
+        }
+    }
+
+    private fun callingRequestApi(searchText: String) {
+        viewModel.requestNewsApi(searchText)
+    }
+
+    private fun setupAdapter(resultNewsOfRequest: ModelNews?) {
+        Log.i(TAG, "setupAdapter: $resultNewsOfRequest")
     }
 }
